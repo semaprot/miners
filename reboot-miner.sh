3a758b457f604864ae17146571f7861e80d29eef
SemaProt <prot.sema2016@gmail.com)>
2017-12-27 13:29:48 +0200

    Dockerfile added

--- reboot-miner.sh
+++ reboot-miner.sh
@@ -2,5 +2,6 @@
 
 ls /home/tftp/pxelinux.cfg/ | grep ^01 | awk -F '01-' '{system("cat /home/tftp/pxelinux.cfg/"$0" | grep 'cloud-'"); gsub("-",":",$2); system("cat /var/lib/misc/dnsmasq.leases | grep "$2); print "\n"}' > minersList
 
-OUTPUT="$(/usr/local/bin/node /miners/index.js)"
-echo "${OUTPUT}"
+echo '' > ~/.ssh/known_hosts
+
+node index.js | awk -F '\n' '{system("ssh -oStrictHostKeyChecking=no rancher@"$0" sudo reboot")}'
