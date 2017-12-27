02cead9abfb06984838a5bfa6eb2b65b1c589588
SemaProt <prot.sema2016@gmail.com)>
2017-12-27 15:42:11 +0200

    buildImage.sh added

---
+++ Docker/buildImage.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+docker rmi kupolua/miners-services
+docker rmi miners-services
+
+containerID="$(docker build -t miners-services . | awk -F' ' '{if(match($1, /^Successfully/)) print $3}')"
+docker tag $containerID kupolua/miners-services:latest
+docker push kupolua/miners-services
